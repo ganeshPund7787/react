4ff2541cf2cb3f3c18cf2470323d1e719c753bdf
Ganesh Pund <ganupund7787@gmail.com>
2024-05-03 14:47:39 -0700

    BrowserRoutes

---
+++ 2. React Routers/02BrowserRouterMethod/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
